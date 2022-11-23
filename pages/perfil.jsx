6b9685e406b926e5b67649ea6dca6ef7ee71787c
AlexRodriguezZA <alekeyn1999@gmail.com>
2022-11-23 20:09:40 -0300

    cambios

--- pages/perfil.jsx
+++ pages/perfil.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSession } from 'next-auth/react'
+import style from "../styles/perfil.module.css"
 
 const perfil = () => {
   const {data: session} = useSession()
