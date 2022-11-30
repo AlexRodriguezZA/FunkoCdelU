658453ee90ccbf147e72f119275f27ed358b7e91
AlexRodriguezZA <alekeyn1999@gmail.com>
2022-11-30 15:46:17 -0300

    Arreglando el grid de categorias

---
+++ components/Generales/GridLayourCategorias.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import style from "../styles/GridLayourCategorias.module.css"
+import Image from 'next/image'
+import marvel_image from "../../assets/Funko_Categorias/marvel.png"
+import peliculas_image from "../../assets/Funko_Categorias/peliculas.png"
+const GridLayourCategorias = () => {
+  return (
+    <div className={style.angry_grid}>
+        <div className={style.item_0}>
+            <h1>Algunas de nuestras categorías</h1>
+        </div>
+        <div className={style.item_1} >
+          <h3 className={style.titulo}>Marvel</h3>
+          <div className={style.image_container}>
+            <Image src={marvel_image} className={style.img_marvel}/>
+          </div>
+        </div>
+        <div className={style.item_2} >
+        <h3 className={style.titulo}>Películas</h3>
+
+            <div className={style.image_container_peliculas}>
+              <Image src={peliculas_image} className={style.img_peliculas}/>
+            </div>
+        </div>
+        <div className={style.item_3} >
+          <h3 className={style.titulo}>Dc comics</h3>
+          <div className={style.image_container_dc}>
+              <Image src={peliculas_image} className={style.img_dc}/>
+            </div>
+        </div>
+        <div className={style.item_4}>
+        <h3 className={style.titulo}>Deportes</h3>
+          <div className={style.image_container_deportes}>
+              <Image src={peliculas_image} className={style.img_deportes}/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default GridLayourCategorias
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import style from "../styles/Home.module.css"
 import Header from '../components/Generales/Header'
 import ListCardsProd from '../components/Generales/ListCardsProd'
-
+import GridLayourCategorias from '../components/Generales/GridLayourCategorias'
 const Home = () => {
   return (
       <div className={style.home_container}>
@@ -11,6 +11,7 @@ const Home = () => {
           <h1 className={style.title_list_card}>Algunos de nuestros <span className={style.span}>Funkos</span></h1>
         </div>
         <ListCardsProd/>
+        <GridLayourCategorias/>
       </div>
       
       
