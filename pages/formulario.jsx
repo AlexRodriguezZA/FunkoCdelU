4a34c7543f60a8389a1a4bdc23d6d15bc46066d1
AlexRodriguezZA <alekeyn1999@gmail.com>
2022-12-01 19:21:05 -0300

    Diseño de formulario terminado

--- pages/formulario.jsx
+++ pages/formulario.jsx
@@ -13,27 +13,28 @@ const formulario = () => {
         </div>
 
         <section className={style.leyenda_formulario}>
-          <h3>Ayudanos a facilitar el envio con tus datos</h3>
+          <h3>Ayudanos a facilitar el envio ingresando tus datos</h3>
         </section>
-        <div className={style.contenedores_inputs}>
-          <label htmlFor="">Dni</label>
-          <input className={style.inputs} placeholder='Ingrese su dni' type="number" name="" id="" />
+
+        <div className={style.conteiner_input_dni}>
+          <label htmlFor="">DNI</label>
+          <input className={style.input_dni} placeholder='Ingrese su DNI' type="number" name="" id="" />
         </div>
         
-        <div className={style.contenedores_inputs}>
+        <div className={style.conteiner_input_ciudad}>
           <label htmlFor="">Ciudad</label>
-          <select name="" id="" className={style.input}>
+          <select name="" id="" className={style.input_ciudad}>
             <option value="">Concepcion del uruguay</option>
             <option value="">Rosario del tala</option>
     
           </select>
         </div>
         
-        <div className={style.contenedores_inputs}>
+        <div className={style.conteiner_input_domicilio}>
           <label htmlFor="">Domicilio: </label>
-          <div>
-            <input placeholder='Domicilio' type="text" name="" id="" />
-            <input placeholder='altura/ nro dpto' type="text" name="" id="" />
+          <div className={style.container_domicilio}>
+            <input className={style.input_domicilio} placeholder='Domicilio' type="text" name="" id="" />
+            <input className={style.input_domicilio} placeholder='altura/ nro dpto' type="number" name="" id="" />
           </div>
           
         </div>
