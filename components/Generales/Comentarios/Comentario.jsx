821562c687741e4f7154ebdb0c115af0187935b9
AlexRodriguezZA <alekeyn1999@gmail.com>
2022-12-05 19:03:51 -0300

    Creando seccion de comentarios

--- components/Generales/Comentarios/Comentario.jsx
+++ components/Generales/Comentarios/Comentario.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import style from "../../styles/Comentario.module.css"
 
 const Comentario = () => {
   return (
-    <div>Comentario</div>
+    <div className={style.comentario_container}>
+      <section>
+        <img src="" alt="Imagen usuario" />
+      </section>
+      <section className={style.comentario_card}>
+        <div className={style.comentario_card_datos_user}>
+          <h3>Nombre</h3>
+          <h5>Fecha</h5>
+        </div>
+
+        <div>
+          <p>Contenido</p>
+        </div>
+      </section>
+    </div>
   )
 }
 
--- components/Generales/Comentarios/Seccion_comentarios.jsx
+++ components/Generales/Comentarios/Seccion_comentarios.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Comentario from './Comentario'
 
 import style from "../../styles/Seccion_comentarios.module.css"
 
@@ -7,19 +8,23 @@ const Seccion_comentarios = () => {
     <div className={style.Seccion_comentario_container}>
 
       <h2 className={style.Seccion_comentario_titulo}>Comentarios</h2>
-      <section>
-        <div>
-          <img src="" alt="" />
-          <input type="text" name="" id="" />
+      <section className={style.comentario_container}>
+        <div className={style.input_comentario_container}>
+          <img src="" alt="Imagen de la usuario" />
+          <textarea className={style.input_comentario} placeholder="Ingrese un comentario..." name="" id="" cols="80" rows="6"></textarea>
         </div>
-        <div>
-          <button>Enviar</button>
-          <button>Cancelar</button>
+        <div className={style.buttons_container}>
+          <button className={style.button_enviar}>Enviar</button>
+          <button className={style.button_cancelar}>Cancelar</button>
         </div>
       </section>
       
-      <section>
-        {/*Targeta de comentarios */}
+      <section className={style.seccion_comentarios_totales}>
+        <Comentario/>
+        <Comentario/>
+        <Comentario/>
+        <Comentario/>
+
       </section>
 
 
