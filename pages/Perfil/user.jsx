69ff1d5909b78c60442451378cc7c0c50b0dbde4
AlexRodriguezZA <alekeyn1999@gmail.com>
2023-02-08 09:52:54 -0300

    Cambios

--- pages/Perfil/user.jsx
+++ pages/Perfil/user.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSession } from 'next-auth/react'
 import style from "../../styles/user.module.css"
+import Link from 'next/link'
 import Image from 'next/image'
 const user = () => {
   const {data: session} = useSession()
@@ -8,7 +9,6 @@ const user = () => {
   if (session) {
     return (
       <div className={style.page_perfil}>
-        {/*NAv interno */}
         <div className={style.card_principal}>
           <div className={style.data_seccion}>
             <section className={style.seccion_1}>
@@ -19,14 +19,16 @@ const user = () => {
             </section>
 
             <section className={style.seccion_2}>
-              <p>Direccion: <span>Rozados 239</span></p>
-              <p>Ciudad: <span>Rosario del tala</span></p>
-              <p>Correo: <span>{session.user.email}</span></p>  
-              <p>Tel: <span>3445 417785</span></p>
+              <p className={style.data_user}>Direccion: <span className={style.data_user_info}>Rozados 239</span></p>
+              <p className={style.data_user}>Ciudad: <span className={style.data_user_info}>Rosario del tala</span></p>
+              <p className={style.data_user}>Correo: <span className={style.data_user_info}>{session.user.email}</span></p>  
+              <p className={style.data_user}>Tel: <span className={style.data_user_info}>3445 417785</span></p>
             </section>
           </div>
           <div className={style.seccion_3}>
             <button className={style.button_editar}>Editar</button>
+            <Link href="/Perfil/compras" className={style.button_compras}>Mis compras</Link>
+
           </div>
         </div>
         
